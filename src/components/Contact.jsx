0fa94a3ff6317a139d031e9d674d3b48ea5075a5
Kim Youngsun <grapelove79@gmail.com>
2025-06-25 00:17:02 +0900

    비디오wrap dvh수정

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,8 +35,10 @@ const Contact = () => {
   return (
     <section id="contact">
       <div className="contact__inner">
-        <h2 className="contact__title contact__motion">연락 <em>Contact</em></h2>
-        <div className="contact__wrap contact__motion">
+        <h2 className="contact__title">연락 <em>Contact</em></h2>
+        {/* <h2 className="contact__title contact__motion">연락 <em>Contact</em></h2> */}
+        <div className="contact__wrap">
+        {/* <div className="contact__wrap contact__motion"> */}
           <p className="contact__desc">제 포트폴리오를 봐주셔서 감사합니다.</p>
           {contactText.map((contact, key) => (
             <div className="contact__mail" key={key}>
