6d8a601eeda048151e9910a9e297ad370e3530bd
Kim Youngsun <grapelove79@gmail.com>
2025-06-24 22:10:49 +0900

    intro수정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -16,7 +16,7 @@ const Intro = () => {
       ScrollTrigger.create({
         // id: "intro" + idx,
         trigger: item,
-        scrub: 0.5,
+        // scrub: 0.5,
         start: "top 90%",
         invalidateOnRefresh: true,
         // markers: true,
