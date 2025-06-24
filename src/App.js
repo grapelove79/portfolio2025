dd53771971f04b7138f0bbfca7314b6ca747c110
Kim Youngsun <grapelove79@gmail.com>
2025-06-25 00:28:24 +0900

    로딩스크린 제거

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoadingScreen from "./components/LoadingScreen";
+// import LoadingScreen from "./components/LoadingScreen";
 import HomeView from './views/HomeView'; // 실제 메인 페이지 컴포넌트
 // import smooth from "./utils/smooth";
 // import { enableLenisAnchors } from "./utils/lenis-anchors";
@@ -8,7 +8,7 @@ import Cursor from "./components/Cursor";
 
 const App = () => {
   // loadingDone: false이면 로딩화면, true이면 메인 콘텐츠 표시
-  const [loadingDone, setLoadingDone] = useState(false);
+  // const [loadingDone, setLoadingDone] = useState(false);
 
   // loadingDone이 App에서 직접 관리되므로 smooth()와 link()도 정확히 로딩 후 한 번만 실행됩니다.
   // useEffect(() => {
@@ -20,18 +20,18 @@ const App = () => {
   return (
     <>
       {/* 로딩이 끝나지 않았으면 로딩 화면만 보여줌 */}
-      {!loadingDone ? (  // loadingDone 이 true 가 아닌 경우 true
+      {/* {!loadingDone ? (  // loadingDone 이 true 가 아닌 경우 true
         <LoadingScreen onFinish={() => {
           setLoadingDone(true);
         }} />
-      ) : (
+      ) : ( */}
       <BrowserRouter>
         <Cursor />
         <Routes>
           <Route path="/" element={<HomeView />} />
         </Routes>
       </BrowserRouter>
-      )}
+       {/* )} */}
     </>
   );
 };
