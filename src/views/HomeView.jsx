d55be310f7c861eb5de2d984ff279bcfc6ff36c7
Kim Youngsun <grapelove79@gmail.com>
2025-06-27 15:50:24 +0900

    homeView에 normalizeScroll 적용

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import Skip from "../components/Skip";
 import IntroSection from "../components/IntroSection";
@@ -9,8 +9,28 @@ import Port from "../components/Port";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const HomeView = () => {
+
+  useEffect(() => {
+    const isIos = () => {
+      return /iP(ad|hone|od)/.test(navigator.userAgent) || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)
+
+    };
+
+    if (!isIos()) {
+      ScrollTrigger.normalizeScroll(true); // iOS 아닌 경우에만 활성화
+    }
+
+    return () => {
+      ScrollTrigger.normalizeScroll(false); //cleanup
+    }
+  }, []);
+
   return (
     <>
       <Skip />
